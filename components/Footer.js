a2fbbe2d276708c3c9f21d4f3a3d218abaa81bdd
reycn <oyrx@vip.qq.com>
2023-03-16 00:49:53 +0800

    Update dep

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,21 @@
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
 import BLOG from '@/blog.config'
-// import Vercel from '@/components/Vercel'
+import Vercel from '@/components/Vercel'
 const Footer = ({ fullWidth }) => {
-	const d = new Date()
-	const y = d.getFullYear()
-	const from = +BLOG.since
-	return (
-		<div
-			className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${!fullWidth
-				? 'max-w-2xl px-4'
-				: 'px-4 md:px-24'}`}
-		>
-			<hr className="border-gray-200 dark:border-gray-600" />
-			<div className="my-4 text-sm leading-6">
-				<div className="flex align-baseline justify-between flex-wrap">
-					<p>
+  const d = new Date()
+  const y = d.getFullYear()
+  const from = +BLOG.since
+  return (
+    <div
+      className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
+        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
+      }`}
+    >
+      <hr className="border-gray-200 dark:border-gray-600" />
+      <div className="my-4 text-sm leading-6">
+        <div className="flex align-baseline justify-between flex-wrap">
+          <p>
 						{BLOG.author} © {from === y || !from ? y : `${from} - ${y}`}
 					</p>{' '}
 					<a href="https://vercel.com?utm_source=Craigary&utm_campaign=oss">
@@ -34,10 +34,10 @@ const Footer = ({ fullWidth }) => {
 						</svg>Vercel
 					</a>
 					{/* <Vercel /> */}
-				</div>
-			</div>
-		</div>
-	)
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
